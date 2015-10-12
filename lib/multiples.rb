28b1a830d600024d8308e2db6631946909eabbff
staceyko <ko.stacey@gmail.com>
2015-10-12 10:49:01 -0400

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(max)
+  range = (1...max).to_a
+  range.select {|num| num if num % 3 == 0 || num % 5 == 0}
+end
+
+def sum_multiples(max)
+  sum_array = collect_multiples(max)
+  sum_array.inject {|sum, n| sum + n}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(max)
+    @max = max
+  end
+
+  def collect_multiples
+    range = (1...@max).to_a
+    range.select {|num| num if num % 3 == 0 || num % 5 == 0}
+  end
+
+  def sum_multiples
+    sum_array = collect_multiples
+    sum_array.inject {|sum, n| sum + n}
+  end
+end
